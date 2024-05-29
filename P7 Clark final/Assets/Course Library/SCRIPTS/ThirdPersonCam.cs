9b099f5d6abae3f73860b84650787629a0a7b86e
Jayden1911 <740394@student.dusd.net>
2024-05-29 15:24:06 -0700

    need to make player controlls
    made the camera work ??

--- P7 Clark final/Assets/Course Library/SCRIPTS/ThirdPersonCam.cs
+++ P7 Clark final/Assets/Course Library/SCRIPTS/ThirdPersonCam.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class ThirdPersonCam : MonoBehaviour
 {
+    
     
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
+}
