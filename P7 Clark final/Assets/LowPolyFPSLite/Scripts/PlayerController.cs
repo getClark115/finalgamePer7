7ad9feae0a4826e403924d8f30bd570f947d69ad
Rodriguez Millan <740394@student.dusd.net>
2024-06-03 20:59:13 -0700

    finished a script
    finished adding a script and need to fix menu

---
+++ P7 Clark final/Assets/LowPolyFPSLite/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 _input;
+    private CharacterController _characterController;
+    private Vector3 _direction;
+
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _characterController.Move(_direction * speed * Time.deltaTime);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
+    }
+}
+
